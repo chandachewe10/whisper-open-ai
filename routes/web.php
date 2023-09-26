7eee36e1effe2d63a73e1803f985af8af629cc8d
chanda chewe <chewec03@gmail.com>
2023-09-26 14:06:28 +0200

    Added Laravel Socialite

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SocialiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+Route::get('/redirect/{provider}', [SocialiteController::class, 'loginSocial'])
+	->name('socialite.login');
+
+Route::get('/callback/{provider}', [SocialiteController::class, 'callbackSocial']);
 
 require __DIR__.'/auth.php';
